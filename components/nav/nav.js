96d0ac54df095b43e16c4c035ffea79708fa6511
pyd3690 <prenaamd@gmail.com>
2021-08-26 11:03:02 -0500

    first commit

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,14 +1,15 @@
 import {React} from 'react';
 import Button from 'react-bootstrap/Button';
+import Link from 'next/link'
 
 
 export default function Navbar(){
     return (
         <div style={{background: 'lightgray', padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
             <h4>CodeNotes</h4>
-            <a href="/api/auth/logout">
+            <Link href="/api/auth/logout" passHref>
                 <Button variant="dark" id="logout" style={{display: 'none'}}>Logout</Button>
-            </a>
+            </Link>
         </div>
     )
 }
